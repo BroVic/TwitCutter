8849c85ed067c76c16b42a235fbc2861afd88cd1
Victor Ordu <victorordu@outlook.com>
2018-11-06 16:31:50 +0100

    Reorganised directory tree

--- Twitcutter/src/base/cmdmain.cpp
+++ Twitcutter/src/base/cmdmain.cpp
@@ -18,9 +18,14 @@
 #include "print.h"
 #include "post.h"
 
+#if _WIN32
+#if _MSC_VER >= 1900
+// Redefine standard streams in newer compiler versions
 FILE _iob[] = { *stdin, *stdout, *stderr };
-extern "C" FILE * __cdecl __iob_func(void) { return _iob; }
+extern "C" FILE* __cdecl __iob_func(void) { return _iob; }
 extern "C" void __imp__set_output_format(void) {};
+#endif
+#endif
 
 int main(int argc, char** argv)
 {
